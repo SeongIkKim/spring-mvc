09d0133b208c8a6c218da2a8d1086fa40159b1f1
SeongIkKim <kpic1638@gmail.com>
2023-08-25 09:13:13 +0900

    feat: add basic servlet and html

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 패키지 내의 서블릿을 스캔하여 실행할 수 있도록 등록
 @SpringBootApplication
 public class ServletApplication {
 
