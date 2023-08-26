796ce5bc02a00dc5feabb06a4552c7ea9e973d4a
SeongIkKim <kpic1638@gmail.com>
2023-08-27 01:13:38 +0900

    fix: fix new-form URL path & members.jsp

--- src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class MvcMemberFormServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String viewPath = "/WEB-INF/views/new-form.jsp";
+        String viewPath = "/WEB_INF/views/new-form.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath); // dispatcher : 다른 서블릿이나 JSP로 이동할 수 있는 기능. 서버 내에서 다시 호출이 발생(redirect와 다름. 브라우저상의 주소가 바뀌지 않음)
         dispatcher.forward(request, response); // 컨트롤러에서 jsp 호출
 
