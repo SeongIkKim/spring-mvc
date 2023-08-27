29027db87bc1ce7a2bee8f9fdc658fa869f54df6
SeongIkKim <kpic1638@gmail.com>
2023-08-27 13:58:29 +0900

    feat: spring mvc old structure

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 패키지 내의 서블릿을 스캔하여 실행할 수 있도록 등록
 @SpringBootApplication
@@ -12,4 +15,9 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
+	// application.properties에 세팅해두면 실제로는 아래의 코드가 실행된것과 같음
+//	@Bean
+//	ViewResolver internalResourceViewResolver() {
+//		return new InternalResourceViewResolver("/WEB_INF/views/", ".jsp");
+//	}
 }
